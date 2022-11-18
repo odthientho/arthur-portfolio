3f9e34a17a634a1403c9e583a503da092c8067cf
Arthur <odthientho@gmail.com>
2022-11-17 21:11:51 -0500

    initial react app setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Welcome from './components/Welcome';
+
+function App() {
+
+  // We return the Welcome component and pass loggedIn and setLoggedIn as props.
+  return <Welcome />;
+}
+
+export default App;
---
+++ src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Here we destructure our props into their own distinct variables
+export default function Welcome({ loggedIn, setLoggedIn }) {
+  // Lets log our our loggedIn variable to see it change in real time
+  console.log('Welcome -> loggedIn', loggedIn);
+
+  // If we are loggedIn render one set of elements, and if not we render another
+  return (
+    <div>
+      My Portfolio
+    </div>
+  );
+}
